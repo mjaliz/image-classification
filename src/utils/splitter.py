638388a960279dffccfbfd9c8ebda4ce81ad33be
Mohammad Javad Alizadeh <mrph14@gmail.com>
2023-03-12 14:59:43 +0330

    anomaly detection some changes

--- src/utils/splitter.py
+++ src/utils/splitter.py
@@ -3,7 +3,7 @@ import shutil
 
 
 class Splitter:
-    def __init__(self, input_path, balance: bool):
+    def __init__(self, input_path, balance: bool, ratio: float = 0.8):
         self.accepted_path = os.path.join(input_path, "accepted")
         self.rejected_path = os.path.join(input_path, "rejected")
         self.root_path = os.path.join(input_path, "..", "..")
@@ -29,10 +29,10 @@ class Splitter:
         self.rejected_list = os.listdir(self.rejected_path)
         self.rejected_list_len = len(self.rejected_list)
 
-        self.valid_test_len = round(self.rejected_list_len * 0.1)
-        self.train_len = round(self.rejected_list_len * 0.8)
+        self.valid_test_len = round(self.rejected_list_len * 0.2)
+        self.train_len = round(self.rejected_list_len * ratio)
         if not balance:
-            self.train_len = round(self.accepted_list_len * 0.8)
+            self.train_len = round(self.accepted_list_len * ratio)
 
         os.makedirs(self.train_accepted_path)
         os.makedirs(self.train_rejected_path)
@@ -82,12 +82,12 @@ class Splitter:
     def train_test_split(self):
         print("*" * 10, "splitting images", "*" * 10)
         self.__valid_split()
-        self.__test_split()
+        # self.__test_split()
         self.__train_split()
 
 
 if __name__ == '__main__':
     current_path = os.path.dirname(os.path.realpath(__file__))
     input_dir = os.path.join(current_path, "..", "..", "..", "dataset", "avatars", "tmp2")
-    splitter = Splitter(input_dir, False)
+    splitter = Splitter(input_dir, False, 0.1)
     splitter.train_test_split()
